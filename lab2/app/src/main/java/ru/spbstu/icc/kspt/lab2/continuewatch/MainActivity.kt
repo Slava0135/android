8569c949c4094d207bfd92dec499f9b08913921b
slava0135 <super.novalskiy_0135@inbox.ru>
2021-10-05 15:44:00 +0300

    Initial commit

---
+++ lab2/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -0,0 +1,53 @@
+package ru.spbstu.icc.kspt.lab2.continuewatch
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    val STATE_SECONDS = "secondsElapsed"
+    var secondsElapsed: Int = 0
+
+    var isWorking = false
+
+    lateinit var textSecondsElapsed: TextView
+
+    var backgroundThread = Thread {
+        while (true) {
+            Thread.sleep(1000)
+            if (isWorking) {
+                textSecondsElapsed.post {
+                    textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        backgroundThread.start()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        isWorking = false
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(STATE_SECONDS, secondsElapsed)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        secondsElapsed = savedInstanceState.getInt(STATE_SECONDS)
+    }
+}
---
+++ lab2/app2/src/main/java/io/slava0135/lab2/app2/MainActivity2.kt
@@ -0,0 +1,53 @@
+package io.slava0135.lab2.app2
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    val STATE_SECONDS = "secondsElapsed"
+    var secondsElapsed: Int = 0
+
+    var isWorking = false
+
+    lateinit var textSecondsElapsed: TextView
+
+    var backgroundThread = Thread {
+        while (true) {
+            Thread.sleep(1000)
+            if (isWorking) {
+                textSecondsElapsed.post {
+                    textSecondsElapsed.setText("Seconds elapsed: " + secondsElapsed++)
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
+        backgroundThread.start()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        isWorking = false
+        getPreferences(MODE_PRIVATE)?.let {
+            with (it.edit()) {
+                putInt(STATE_SECONDS, secondsElapsed)
+                apply()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        isWorking = true
+        getPreferences(MODE_PRIVATE)?.let {
+            secondsElapsed = it.getInt(STATE_SECONDS, secondsElapsed)
+        }
+    }
+}
