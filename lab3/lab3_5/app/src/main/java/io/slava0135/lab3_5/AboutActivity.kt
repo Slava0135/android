8a1ea4b7280b642293352020144231ad7e46cd0d
slava0135 <super.novalskiy_0135@inbox.ru>
2021-11-08 20:59:20 +0300

    lab3_5 update

--- lab3/lab3_5/app/src/main/java/io/slava0135/lab3_5/AboutActivity.kt
+++ lab3/lab3_5/app/src/main/java/io/slava0135/lab3_5/AboutActivity.kt
@@ -3,7 +3,7 @@ package io.slava0135.lab3_5
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class About : AppCompatActivity() {
+class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
